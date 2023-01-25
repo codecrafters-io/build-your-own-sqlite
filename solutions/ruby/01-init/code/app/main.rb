27779b93bf750c2a2427a4bc9680120b4c98a312
Ben Garcia <49890553+benngarcia@users.noreply.github.com>
2023-01-24 20:34:37 -0500

    Think this adds ruby support

---
+++ solutions/ruby/01-init/code/app/main.rb
@@ -0,0 +1,12 @@
+database_file_path = ARGV[0]
+command = ARGV[1]
+
+if command == ".dbinfo"
+  File.open(database_file_path, "rb") do |database_file|
+    database_file.seek(16)  # Skip the first 16 bytes of the header
+    page_size = database_file.read(2).unpack("n")[0]
+    puts "database page size: #{page_size}"
+  end
+else
+  puts "Invalid command: #{command}"
+end
