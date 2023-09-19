e9aee4b21a48b4cba17ae37c4dc2807bca69a5e8
libmartinito <lib.martinito@pm.me>
2023-09-20 04:10:02 +0800

    CC-741 added compiled zig files

---
+++ solutions/zig/01-init/code/your_sqlite3.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# DON'T EDIT THIS!
+#
+# CodeCrafters uses this file to test your code. Don't make any changes here!
+#
+# DON'T EDIT THIS!
+set -e
+zig build-exe ./app/main.zig
+./main "$@"
