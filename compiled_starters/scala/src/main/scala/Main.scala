59fb9bbbc816b625f71b47cbb034921213cc3f5a
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2025-03-27 19:45:35 -0400

    Add compiled starter for scala

--- compiled_starters/scala/src/main/scala/Main.scala
+++ compiled_starters/scala/src/main/scala/Main.scala
@@ -19,12 +19,7 @@ object Main extends App {
       databaseFile.read(pageSizeBytes)
       val pageSizeSigned = ByteBuffer.wrap(pageSizeBytes).getShort()
       val pageSize = pageSizeSigned & 0xFFFF
-
-      // You can use print statements as follows for debugging, they'll be visible when running tests.
-      System.err.println("Logs from your program will appear here!")
-
-      // Uncomment this block to pass the first stage
-      // println("database page size: " + pageSize)
+      println("database page size: " + pageSize)
     }
     case _ => println("Missing or invalid command passed: " + command)
   }
