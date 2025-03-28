b91f12f0950fd5a71bff5df075e3904716f5f967
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2025-03-28 08:41:47 -0400

    Use Using class for resource management

--- starter_templates/scala/code/src/main/scala/Main.scala
+++ starter_templates/scala/code/src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import java.io.File
 import java.io.FileInputStream
 import java.io.IOException
 import java.nio.ByteBuffer
+import scala.util.Using
 
 object Main extends App {
   if args.length < 2
@@ -13,13 +14,18 @@ object Main extends App {
   val command = args(1);
   command match {
     case ".dbinfo" => {
-      val databaseFile = new FileInputStream(new File(databaseFilePath))
-      databaseFile.skip(16)
-      val pageSizeBytes = new Array[Byte](2)
-      databaseFile.read(pageSizeBytes)
-      val pageSizeSigned = ByteBuffer.wrap(pageSizeBytes).getShort()
-      val pageSize = pageSizeSigned & 0xFFFF
-      println("database page size: " + pageSize)
+      Using(new FileInputStream(new File(databaseFilePath))) { databaseFile =>
+        databaseFile.skip(16)
+        val pageSizeBytes = new Array[Byte](2)
+        databaseFile.read(pageSizeBytes)
+        val pageSizeSigned = ByteBuffer.wrap(pageSizeBytes).getShort()
+        val pageSize = pageSizeSigned & 0xFFFF
+        println("database page size: " + pageSize)
+      }.recover {
+        case e: IOException =>
+          println(s"Error reading database file: ${e.getMessage}")
+          System.exit(1)
+      }
     }
     case _ => println("Missing or invalid command passed: " + command)
   }
